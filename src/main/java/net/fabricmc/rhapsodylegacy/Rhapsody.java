91f96759b2368e9fdd13ca9fb7e505e274cfb2ef
cocona20xx <pexium128@gmail.com>
2021-01-24 20:10:24 -0500

    Initial commit 2: electric boogaloo

---
+++ src/main/java/net/fabricmc/rhapsodylegacy/Rhapsody.java
@@ -0,0 +1,11 @@
+package net.fabricmc.rhapsodylegacy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Rhapsody implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
